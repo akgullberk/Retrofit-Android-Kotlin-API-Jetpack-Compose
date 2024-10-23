b9d19543cbd0a77ed7c28ffe9ca22e6e3fa5fe4b
Berk Akgül <berkak_gul51@hotmail.com>
2024-10-23 15:30:00 +0300

    TopBar

--- app/src/main/java/com/example/retrofitcompose/MainActivity.kt
+++ app/src/main/java/com/example/retrofitcompose/MainActivity.kt
@@ -6,44 +6,105 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.retrofitcompose.model.CryptoModel
+import com.example.retrofitcompose.service.CryptoAPI
 import com.example.retrofitcompose.ui.theme.RetrofitComposeTheme
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class MainActivity : ComponentActivity() {
 
-    private val BASE_URL = "https://raw.githubusercontent.com/"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RetrofitComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MainScreen() {
+
+    val BASE_URL = "https://raw.githubusercontent.com/"
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(CryptoAPI::class.java)
+
+    val call = retrofit.getData()
+
+    call.enqueue(object : Callback<List<CryptoModel>>{
+        override fun onResponse(
+            call: Call<List<CryptoModel>>,
+            response: Response<List<CryptoModel>>
+        ) {
+            if(response.isSuccessful){
+                response.body()?.let {
+                    it.forEach {
+                        println(it.currency)
+                    }
+                }
+            }
+        }
+
+        override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
+            t.printStackTrace()
+        }
+
+    })
+
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Retrofit Compose",Modifier.padding(10.dp), fontSize = 26.sp) },
+
+
+
+                )
+
+        },
+        content = { paddingValues ->
+            // Ekrandaki diğer içerik burada olacak
+            // Padding değerleri kullanılarak içerik üst çubuk altında başlatılır.
+            Text(
+                text = "Ana İçerik",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            )
+        }
     )
 }
+
+
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     RetrofitComposeTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
